9b126195513d41a75d157cf98eedea88d4363f20
Prashant Kumar <prashant@playsimple.in>
2018-05-13 15:10:22 +0530

    added unit test for car class

---
+++ tests/test_car.py
@@ -0,0 +1,21 @@
+import unittest
+from src.car import Car
+
+class TestCar(unittest.TestCase):
+    def setUp(self):
+        self.car = Car("123", "Red")
+
+    def test_color(self):
+        self.assertEqual(self.car.get_color(), 'Red')
+
+    def test_reg_number(self):
+        self.assertEqual(self.car.get_registration_number(), '123')
+
+    def test_slot_number(self):
+        self.assertEqual(self.car.get_slot(), -1)
+        self.car.assign_slot(1)
+        self.assertEqual(self.car.get_slot(), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
