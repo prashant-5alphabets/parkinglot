28901368106fb1da1048e23360b74795156cac78
Prashant Kumar <prashant@playsimple.in>
2018-05-13 16:20:15 +0530

    changed car class remove slot number dependency

--- src/car.py
+++ src/car.py
@@ -2,14 +2,6 @@ class Car:
     def __init__(self, registration_number, color):
         self.registration_number = registration_number
         self.color = color
-        self.slot = -1
-
-    def assign_slot(self, slot):
-        self.slot = slot
-        return
-
-    def get_slot(self):
-        return self.slot
 
     def get_color(self):
         return self.color
