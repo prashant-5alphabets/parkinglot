e1af0252665d28a65daf5c8b0a76438605e3227a
Prashant Kumar <prashant@playsimple.in>
2018-05-13 16:41:56 +0530

    removed is_empty condition as it unneccessary

--- src/parking_lot.py
+++ src/parking_lot.py
@@ -29,38 +29,35 @@ class ParkingLot:
     def get_reg_of_cars_of_particular_colors(self, color):
         reg_nums = []
         for slot in self.slots:
-            if not slot.is_empty():
-                reg_num, _color = slot.get_car_parked()
-                if _color == color:
-                    reg_nums.append(reg_num)
-        print ",".join(reg_nums)
+            reg_num, _color = slot.get_car_parked()
+            if _color == color:
+                reg_nums.append(reg_num)
+        print ", ".join(reg_nums)
 
     # this function will returns Slot number in which a car with a given registration number is parked.
     def get_slot_by_reg_number(self, registration_number):
         for slot in self.slots:
-            if not slot.is_empty():
-                reg_num, _color = slot.get_car_parked()
-                if reg_num == registration_number:
-                    print slot.get_slot_number()
-                    return
+            reg_num, _color = slot.get_car_parked()
+            if reg_num == registration_number:
+                print slot.get_slot_number()
+                return
         print "Not found"
 
     #this function will return Slot numbers of all slots where a car of a particular colour is parked.
     def get_slots_by_color(self, color):
         slots = []
         for slot in self.slots:
-            if not slot.is_empty():
-                reg_num, _color = slot.get_car_parked()
-                if _color == color:
-                    slots.append(reg_num)
-        print ",".join(slots)
+            reg_num, _color = slot.get_car_parked()
+            if _color == color:
+                slots.append(str(slot.get_slot_number()))
+        print ", ".join(slots)
 
     def show_status(self):
         print "Slot No. Registration No Colour"
         for slot in self.slots:
             if not slot.is_empty():
                 registration_number, color = slot.get_car_parked()
-                print slot.get_slot_number()+1, registration_number, color
+                print slot.get_slot_number(), registration_number, color
 
 
 if __name__ == '__main__':
@@ -70,11 +67,11 @@ if __name__ == '__main__':
             input_string = raw_input()
             input_array = input_string.split(" ")
             if input_array[0] == 'create_parking_lot':
-                parking_lot.create_par_king_lot(input_array[1])
+                parking_lot.create_par_king_lot(int(input_array[1]))
             elif input_array[0] == 'park':
                 parking_lot.park_car(input_array[1], input_array[2])
             elif input_array[0] == 'leave':
-                parking_lot.empty_slot(input_array[1])
+                parking_lot.empty_slot(int(input_array[1]))
             elif input_array[0] == 'status':
                 parking_lot.show_status()
             elif input_array[0] == 'registration_numbers_for_cars_with_colour':
@@ -86,6 +83,7 @@ if __name__ == '__main__':
             else:
                 print "Command not found"
         except Exception as e:
+            print e
             print "some error occured !!!"
 
 
